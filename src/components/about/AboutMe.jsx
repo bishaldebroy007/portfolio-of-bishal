c4029084a85a635f1aea6ce2f723732e434ac0f2
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-06-27 13:33:48 +0600

    favicon icon changed

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const AboutMe = () => {
     return (
-        <div className='bg-white text-black min-h-[500px] p-30'>
-            <div className='flex justify-center text-5xl font-semibold'>
+        <div className='bg-white text-black min-h-[500px] px-30 py-40'>
+            <div className='flex justify-center text-5xl font-semibold py-3'>
                 <h1>About Me</h1>
             </div>
-            <div className="mockup-code w-full rounded-4xl text-xl mt-10">
+            <div className="mockup-code w-full rounded-4xl text-xl mt-5">
                 <pre data-prefix="$"><code>yarn add bishal</code></pre>
                 <pre data-prefix=">" className="text-warning"><code>installing...</code></pre>
                 <pre data-prefix=">" className="text-purple-400"><code>Passionate about Front-End Development (HTML, CSS, JS) and expanding into full-stack with React,</code></pre>
@@ -14,7 +14,7 @@ const AboutMe = () => {
                 <pre data-prefix=">" className="text-purple-400"><code>With a research background in Human-Computer Interaction (HCI), I focus on enhancing user experiences through innovation.</code></pre>
                 <pre data-prefix=">" className="text-purple-400"><code>A lifelong learner and collaborative leader, I thrive in dynamic environments, driving projects with creativity and a growth mindset.</code></pre>
                 <pre data-prefix=">" className="text-purple-400"><code>Let's shape the future of tech together!</code></pre>
-                <pre data-prefix=">" className="text-success"><code>Done!</code></pre>
+                <pre data-prefix=">" className="text-success"><code>Done in 0s 64ms</code></pre>
             </div>
         </div>
     )
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,12 +11,12 @@ const Projects = () => {
             <div className='flex justify-center text-5xl font-semibold mt-10 mb-3'>
                 <h1>Projects</h1>
             </div>
-            <div className="flex flex-wrap justify-center gap-8 p-4 m-20">
+            <div className="flex flex-wrap justify-center gap-8 p-4 m-20 hover:opacity-500 transition-opacity duration-300">
 
                 {projects.map((project) => (
                     <div
                         key={project.id}
-                        className="card bg-base image-full w-150 transition-shadow"
+                        className="card bg-base-100 image-full w-150 transition-shadow"
                     >
                         <figure>
                             <img
@@ -26,7 +26,7 @@ const Projects = () => {
                             />
                         </figure>
                         <div className="card-body">
-                            <h2 className="card-title">{project.project_title}</h2>
+                            <h2 className="card-title font-bold">{project.project_title}</h2>
                             <p>{project.project_details}</p>
                             <div className="card-actions justify-end mt-4">
                                 <a
