02c02fc487f1d215aad52e0292d8e3a4f0bb75b7
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-06-27 18:50:36 +0600

    small changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
             <div className="hero bg-base-200 min-h-[750px]">
                 <div className="hero-content text-center">
                     <div className="max-w-md">
-                        <img src="public/Graduation photo.jpg" alt="Bishal Deb Roy" className="rounded-full w-76 h-85 mx-auto mb-6 border-4 border-white hover:border-green-500 hover:scale-110  duration-500" />
+                        <img src="/Graduation photo.jpg" alt="Bishal Deb Roy" className="rounded-full w-76 h-85 mx-auto mb-6 border-4 border-white hover:border-green-500 hover:scale-110  duration-500" />
                         <h1 className="text-5xl font-bold">Bishal Deb Roy</h1>
                         <a
                             href="/path/to/your/resume.pdf" // Replace with actual path
