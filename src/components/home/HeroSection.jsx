266785077277fdf5b15f0fb2269f105ee7e99a8e
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-06-27 18:53:19 +0600

    pdf added

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -10,8 +10,8 @@ const HeroSection = () => {
                         <img src="/Graduation photo.jpg" alt="Bishal Deb Roy" className="rounded-full w-76 h-85 mx-auto mb-6 border-4 border-white hover:border-green-500 hover:scale-110  duration-500" />
                         <h1 className="text-5xl font-bold">Bishal Deb Roy</h1>
                         <a
-                            href="/path/to/your/resume.pdf" // Replace with actual path
-                            download="YourName_Resume.pdf" // Suggested filename for download
+                            href="/Resume_Bishal.pdf" // Replace with actual path
+                            download="Bishal Deb Roy_Resume.pdf" // Suggested filename for download
                             className="btn bg-green-700 hover:scale-105 duration-300 hover:bg-green-500 hover:text-black text-white rounded-xl mt-4 font-normal text-2xl"
                         >
                             Resume
