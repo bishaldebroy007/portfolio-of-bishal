ad921afa00ae3d81324a35fc7449df5fbfdcf9ad
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-07-31 00:33:25 +0600

    about me section improved

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 const AboutMe = () => {
     return (
-        <div id='aboutMe' className='bg-white text-black min-h-[500px] px-30 py-40'>
-            <div className='flex justify-center text-5xl font-semibold py-3'>
-                <h1>About Me</h1>
-            </div>
-            <div className="mockup-code w-full rounded-4xl text-xl mt-5">
-                <pre data-prefix="$"><code>yarn add bishal</code></pre>
-                <pre data-prefix=">" className="text-warning"><code>installing...</code></pre>
-                <pre data-prefix=">" className="text-purple-400 md:text-pretty lg:text-nowrap"><code>Passionate about Front-End Development (HTML, CSS, JS) and expanding into full-stack with React,</code></pre>
-                <pre data-prefix=">" className="text-purple-400 md:text-pretty lg:text-nowrap"><code>I also explore Python, Data Science, and Machine Learning, along with frameworks like Django.</code></pre>
-                <pre data-prefix=">" className="text-purple-400 md:text-pretty lg:text-nowrap"><code>With a research background in Human-Computer Interaction (HCI), I focus on enhancing user experiences through innovation.</code></pre>
-                <pre data-prefix=">" className="text-purple-400 md:text-pretty lg:text-nowrap"><code>A lifelong learner and collaborative leader, I thrive in dynamic environments, driving projects with creativity and a growth mindset.</code></pre>
-                <pre data-prefix=">" className="text-purple-400"><code>Let's shape the future of tech together!</code></pre>
-                <pre data-prefix=">" className="text-success"><code>Done in 0s 64ms</code></pre>
+        <div className="hero bg-green-900 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src="Graduation photo.jpg"
+                    className="max-h-xs max-w-3xs md:max-h-sm md:max-w-sm rounded-2xl shadow-2xl"
+                />
+                <div>
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">About Me</h1>
+                    <p className="py-6 text-base md:text-lg lg:text-xl">
+                        Passionate about Front-End Development (HTML, CSS, JS) and expanding into full-stack with React, I also explore Python, Data Science, and Machine Learning, along with frameworks like, NEXT JS & Django. With a research background in Human-Computer Interaction (HCI), I focus on enhancing user experiences through innovation. A lifelong learner and collaborative leader, I thrive in dynamic environments, driving projects with creativity and a growth mindset. Let's shape the future of tech together!
+                    </p>
+                    <button className="btn btn-info">Learn More</button>
+                </div>
             </div>
         </div>
     )
 }
+
+// Passionate about Front-End Development (HTML, CSS, JS) and expanding into full-stack with React, I also explore Python, Data Science, and Machine Learning, along with frameworks like, NEXT JS & Django. With a research background in Human-Computer Interaction (HCI), I focus on enhancing user experiences through innovation. A lifelong learner and collaborative leader, I thrive in dynamic environments, driving projects with creativity and a growth mindset. Let's shape the future of tech together!
 
 export default AboutMe
