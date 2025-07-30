7359f4e1ec4639e9c78a6f66538fd1bae366728d
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-07-31 02:26:17 +0600

    aboutme section updated

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -13,7 +13,10 @@ const AboutMe = () => {
                     <p className="py-6 pr-2 md:pr-5 text-base md:text-lg lg:text-xl text-wrap">
                         Passionate about Front-End Development (HTML, CSS, JS, TS) and expanding into full-stack with React & NEXT JS, I also explore Python, Data Science, and Machine Learning, along with frameworks like, Django. With a research background in Human-Computer Interaction (HCI), I focus on enhancing user experiences through innovation. A lifelong learner and collaborative leader, I thrive in dynamic environments, driving projects with creativity and a growth mindset. Let's shape the future of tech together!
                     </p>
-                    <button className="btn btn-lg mt-3 rounded-xl text-white hover:outline-2 duration-300">Learn More</button>
+                    <a href="https://www.linkedin.com/in/bishal-deb-roy-0b31241a0/" target="_blank" >
+                        <button className="btn btn-lg mt-3 rounded-xl text-white hover:outline-2 duration-300">Learn More
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
