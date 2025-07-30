b040a8ea317101fe675389f127b644d7564f866d
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-07-31 01:37:13 +0600

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import AboutMe from './components/about/AboutMe';
 import Education from './components/education/Education';
 import Footer from './components/footer/Footer';
@@ -6,27 +5,37 @@ import HeroSection from './components/home/HeroSection';
 import NavBar from './components/home/NavBar';
 import Projects from './components/projects/Projects';
 import Skills from './components/skills/Skills';
-function App() {
+import React, { useState, useEffect } from 'react';
 
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <NavBar />
-
-      <HeroSection />
-
-      <AboutMe />
-
-      <Projects />
-
-      <Skills />
-
-
-      <Education />
-
-      <Footer />
-    </>
-  )
+    <div>
+      {isLoading ? (
+        <div className="grid bg-green-100 grid-rows-1 items-center justify-center h-screen w-full">
+          <span className="loading loading-dots text-black loading-xl"></span>
+        </div>
+      ) : (
+        <>
+          <NavBar />
+          <HeroSection />
+          <AboutMe />
+          <Projects />
+          <Skills />
+          <Education />
+          <Footer />
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
